e9e8bb786f75da60a393b34751b17af1b42cdaa2
roberto.boston <roberto.bozzo97@tiscali.it>
2023-03-02 18:59:25 +0100

    show done

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -5,9 +5,19 @@
         <div class="row justify-content-center">
             <div class="col-12 py-3">
                 <div class="d-flex justify-content-between align-items-center">
-                    <h1>Visualizzazione Comic {{$comic['title']}}</h1>
+                    <h1>{{$comic['title']}}</h1>
                     <a href="{{route('comics.index')}}" class="btn btn-primary">Torna all''elenco</a>
                 </div>
+            </div>
+            <div>
+                <img src="{{$comic['thumb']}}" alt="">
+            </div>
+            <div>
+                <p>{{$comic['description']}}</p>
+                <h3>{{$comic['price']}}$</h3>
+                <h3>Data di uscita: {{$comic['sale_date']}}</h3>
+                <h3>Writers: {{$comic['writers']}}</h3>
+                <h3>Artists: {{$comic['artists']}}</h3>
             </div>
         </div>
     </div>
